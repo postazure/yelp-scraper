ab3d8f6a9bb76a309b64da609f85a04c0dc5580c
Max Gordon <gordonmaxc@gmail.com>
2015-01-08 14:07:54 -0800

    improved review

--- lib/review.rb
+++ lib/review.rb
@@ -1,12 +1,24 @@
 class Review
   attr_reader :text_content, :date, :star_rating, :author, :review_html
-  def initialize(nokogiri_object, review_num = 0)
-    review = nokogiri_object.css("ul.reviews").css("li")[review_num]
+  def initialize(noko, review_num = 0)
+    @noko = noko
+    @review_num = review_num
+    @text_content = extract_content.css("div.review-content p").text
+    @date = extract_meta("datePublished")
+    @star_rating = extract_meta("ratingValue")
+    @author = extract_meta("author")
+    @review_html = extract_content.to_html
+  end
 
-    @text_content = review.css(".review-content p").text
-    @date = review.css("meta[itemprop='datePublished']").attr("content").value
-    @star_rating = review.css("meta[itemprop='ratingValue']").attr("content").value
-    @author = review.css("meta[itemprop='author']").attr("content").value
-    @review_html = review.to_html
+  def five_star?
+    star_rating.to_i == 5
+  end
+
+  private
+  def extract_content
+    @noko.css("ul.reviews").css("li")[0]
+  end
+  def extract_meta(property)
+    extract_content.css("meta[itemprop='#{property}']").attr("content").value
   end
 end
