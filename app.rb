5c37156efc992ba91e033c964adf8a18e38bdd97
Max Gordon <gordonmaxc@gmail.com>
2015-01-08 16:10:49 -0800

    data from web

--- app.rb
+++ app.rb
@@ -1,26 +1,29 @@
 require "Nokogiri"
+require "RestClient"
 require_relative "./lib/noko_parser"
 require_relative "./lib/review"
+web_data = RestClient.get("http://www.yelp.com/biz/fat-angel-san-francisco", user_agent: "Chrome")
+# web_data = RestClient.get("http://www.google.com")
+noko_file = NokoParser.new(web_data)
+# noko_file = NokoParser.new(File.open("./data/fat-angel.html"))
+noko_reviews = noko_file.reviews
 
-noko_reviews = NokoParser.new("./data/fat-angel.html").reviews
 review_objects = noko_reviews.map do |noko_review|
-  Review.new(noko_review)
+ Review.new(noko_review)
 end
-
 def star_ratings_brakedown(data)
-  stars = {
-    "1.0" => 0,
-    "2.0" => 0,
-    "3.0" => 0,
-    "4.0" => 0,
-    "5.0" => 0,
-  }
-
-  data.each do |review|
-    stars[review.star_rating] = stars[review.star_rating].to_i + 1
-  end
-
-  stars
+ stars = {
+   "1.0" => 0,
+   "2.0" => 0,
+   "3.0" => 0,
+   "4.0" => 0,
+   "5.0" => 0,
+ }
+ data.each do |review|
+   stars[review.star_rating] = stars[review.star_rating].to_i + 1
+ end
+ stars
 end
-
+# p noko_file.details
 p star_ratings_brakedown(review_objects)
+# puts review_objects.count
--- lib/noko_parser.rb
+++ lib/noko_parser.rb
@@ -1,10 +1,11 @@
 class NokoParser
-  def initialize(file)
-    f = File.open(file)
-    @data = Nokogiri::HTML(f)
+  def initialize(source)
+    @data = Nokogiri::HTML(source)
   end
   def reviews
-    data = @data.css("ul.reviews > li").map {|review| review}
-    data
+    @data.css("ul.reviews > li").map {|review| review}
+  end
+  def details
+    @data.css("rating-details-histogram")
   end
 end
